78781e0ffc212cb0526c242baa799021cb22e503
Hannah2020M <coder2020aug@gmail.com>
2022-02-28 23:04:50 -0600

    AlteredStatusBkgrndCOlor added to file scriptJS

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load',function(){
+let takeOffBtn = dcument.getElementById();
+takeOffBtn.addEventListener('click',function(){
+let isReadyToLaunch = window.confirm("Confirm that the shuttle is ready for takeoff.");
+  if(isReadyToLaunch{
+    let flightStatus = document.getElementById("flightStatus");
+    flightStatus.innerText = "Shuttle in flight.";
+    let shuttleFlightScreen = document.getElementById("ShuttleBackground");
+    shuttleFlightScreen.setAttribute("style","background-color: #009933");
+}
+});
+
+});
